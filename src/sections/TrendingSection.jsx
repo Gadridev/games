003c9ab0c84e07658fd274cc175b31173df78c38
imane imziline <imaneimzline2@gmail.com>
2026-05-05 16:33:39 +0100

    implement TrendingSection with API fetch and navigation

--- src/sections/TrendingSection.jsx
+++ src/sections/TrendingSection.jsx
@@ -1,9 +1,29 @@
-function TrendingSection() {
-    return (
-        <div>
-            <h1>trending</h1>
-        </div>
-    )
+import { useNavigate } from "react-router-dom";
+import GameCard from "../components/GameCard";
+
+function TrendingSection({ games }) {
+  const navigate = useNavigate();
+
+  if (!games || games.length === 0) {
+    return <p>No trending games</p>;
+  }
+
+  return (
+    <section  className="trending-section">
+      <h2>Trending Games</h2>
+
+      <div className="trending-grid">
+        {games.slice(0, 6).map((game) => (
+          <div
+            key={game.id}
+            onClick={() => navigate(`/games/${game.id}`)}
+          >
+            <GameCard game={game} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default TrendingSection
+export default TrendingSection;
