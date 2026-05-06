2d457beca9477519c9c6054671c7afdf4f1aec43
Gadri Mohamed <gadrimohamed8@gmail.com>
2026-05-06 11:39:44 +0100

    the file structure of pages ischanged

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Games from './pages/Games'
 import GameDetail from './pages/GameDetail'
-import GameDevelopers from './pages/GameDevelopers'
 import './App.css'
+import Creators from './pages/Creators'
+import CreatorDetails from './pages/CreatorDetails'
 
 function App() {
   return (
     <div className="app-content">
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/games" element={<Games />} />
+        <Route path="/creators" element={<Creators />} />
         <Route path="/games/:game_id" element={<GameDetail />} />
-        <Route path="/games/:game_id/developers-team" element={<GameDevelopers />} />
+        <Route path="/creator/:creatorId" element={<CreatorDetails />} />
       </Routes>
     </div>
   )
